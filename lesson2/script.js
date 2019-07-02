ab4120d270df90cb437d189c7d0f1857bbb925fc
SimonovI <webkurs2019@gmail.com>
2019-07-02 15:51:53 +0300

    cosolу

--- lesson2/script.js
+++ lesson2/script.js
@@ -3,10 +3,12 @@ let  money = 50000,
     addExpenses = 'СОДЕРЖАНИЕ ПК, ХОСТИНГ, СОФТ',
     deposit = true,
     mission = 100000,
-    budgetDay = money / 30,
-    period = 2;  
+    //budgetDay = money / 30,
+    period = 2;
+budgetDay = (money / 30);
 console.log(typeof money,typeof income,typeof deposit);
 console.log(income.length);
 console.log(period + " " + "Месяца", mission + " " + 'рублей');
 console.log(addExpenses.toLowerCase().split(", "));
-    console.log(budgetDay);
+//money / 30 = budgetDay;
+console.log(money % 30);
